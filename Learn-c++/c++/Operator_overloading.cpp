31d3daa0ca0dc2ea18401ab551de64925935ee4a
unknown <yerramellijahnavi@gmail.com>
2024-09-21 21:36:49 +0530

    Task:operator overloading

--- Learn-c++/c++/Operator_overloading.cpp
+++ Learn-c++/c++/Operator_overloading.cpp
@@ -1,18 +1,36 @@
 #include <iostream>
 using namespace std;
 
+
+//create a class with the point details
 class point {
 	int a;
 	int b;
 public:
 	point(int a = 0, int b = 0) :a{ a }, b{ b } {};
+
+	//overloading operator '+'
+	point operator +(const point& p) {
+		return point(a + p.a, b + p.b);
+	}
+
+	void print() {
+		cout << "a: " << a << "b: " << b << endl;
+	}
+
 };
 
 int main() {
-	//point p(5);
+	//create 2 objects with point cordinate details
+	point p1(5, 6), p2(4, 5);
+	point p3 = p1 + p2;     //point p3 = p1.operator+(p2)   
+	p3.print();
 
-	int a = 9, b = 7;
+	//sample code with the implicit datatypes using '+' operator
+
+	/*int a = 9, b = 7;
 	int c = a + b;
-	cout << c << endl;
+	cout << c << endl;*/
+
 	return 0;
 }
