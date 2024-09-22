4b86f37b7fd56666eb67e49406b35a9f88fae5bd
unknown <yerramellijahnavi@gmail.com>
2024-09-22 10:46:44 +0530

    Task:operators overloading

--- Learn-c++/c++/Operator_overloading.cpp
+++ Learn-c++/c++/Operator_overloading.cpp
@@ -1,3 +1,5 @@
+#include "subscript_operator-overloading.cpp"
+
 #include <iostream>
 using namespace std;
 
@@ -24,13 +26,19 @@ int main() {
 	//create 2 objects with point cordinate details
 	point p1(5, 6), p2(4, 5);
 	point p3 = p1 + p2;     //point p3 = p1.operator+(p2)   
-	p3.print();
+	//p3.print();
 
 	//sample code with the implicit datatypes using '+' operator
-
 	/*int a = 9, b = 7;
 	int c = a + b;
 	cout << c << endl;*/
+
+	//object for subscript operator overloading
+	subscript point(7,66);
+	point.print();
+	point[0] = 1;
+	point[1] = 2;
+	point.print();
 
 	return 0;
 }
